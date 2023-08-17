d543c9f4f0ecc99dae27ba78f41c462c3a140abb
gobanoff <a.kliosovas@yahoo.com>
2023-08-17 19:36:05 +0300

    prekes

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const home = {
     {
       id: 1,
       title: "Cotton T-shirt",
-      price: 54,
+      price: 64,
       brand: "adidas",
       category: "T-Shirt",
       thumbnail: "https://www.sportsdirect.com/images/products/59592640_l.jpg",
@@ -34,23 +34,21 @@ const cart = document.querySelector("#cart");
 const goods = document.querySelector(".prek");
 
 item1.innerHTML += `<div class="pr"><p>Shopping Cart</p><span>3 items</span></div>
-<div class="prek"></div><a href="#"id="back"onclick="showList()"><img src="arrow.png" alt=""> Back to shop</a>`;
+<div class="prek"></div><a href="#"id="back"onclick="zero()"><img src="arrow.png" alt=""> Back to shop</a>`;
 let pq = [0, 0, 0];
-
+let totalPrice = 0;
 function showList() {
   let html = "";
   pq = [0, 0, 0];
-
+  
   for (let i = 0; i < 3; i++) {
     const title = home.p[i].title;
     const cat = home.p[i].category;
     const price = home.p[i].price;
-    const discount = home.p[i].discountPercentage;
+
     const image = home.p[i].thumbnail;
     const brand = home.p[i].brand;
-    const perc = Math.round(price - (price / 100) * discount);
-    const dis = Math.round(discount);
-
+    totalPrice += home.p[i].price * pq[i];
     html += `
      <div id="prek1">
       <img src="${image}"  alt="">
@@ -62,21 +60,14 @@ function showList() {
       <button id="cross"onclick="delItem(event)"><img src="x-circle.svg"class="crs" alt=""></button> </div><hr>`;
 
     document.querySelector(".prek").innerHTML = html;
-   
-  } 
+  }
 }
 showList();
 
-function delItem(e) {
-  e.target.parentElement.parentElement.remove();
+function delItem(e) { 
+ if( e.target.parentElement.parentElement.remove())totalPrice = totalPrice - home.p[i].price * pq[i];
 }
-const title = home.p[0].title;
-const cat = home.p.category;
-const totalPrice =
-  home.p[1].price * 1 + home.p[0].price * 1 + home.p[2].price * 1;
-const discount = home.p.discountPercentage;
-const image = home.p.thumbnail;
-const brand = home.p.brand;
+
 
 const d = [" ", 5, 8, 0];
 
@@ -85,7 +76,8 @@ const item2 = document.querySelector(".cart2");
 item2.innerHTML += `<div class="forms"><h2>Summary</h2><hr> 
 <div class="sp1"><p>ITEMS3</p><p id="total">€${totalPrice}.00</p> </div>
 <h3 class="ship">SHIPPING</h3><div class="input-group">
-<select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon" onchange="updateTotalPrice()">
+<select class="form-select" id="inputGroupSelect04"
+ aria-label="Example select with button addon" onchange="updateTotalPrice()">
 
   <option value="1">${d[0]} </option>
   <option value="2">DPD express - €${d[1]}.00 </option>
@@ -94,8 +86,9 @@ item2.innerHTML += `<div class="forms"><h2>Summary</h2><hr>
 </select>
 
 </div><h3 class="giv">GIVE CODE</h3>
-<input class="code2"type="text" placeholder="Enter your code                                              &rarr;" onchange="updateDiscountPrice()">
-<div class="line"></div><div class="sp"><p class"ice">TOTALPRICE</p><p id="total1">€${totalPrice}.00</p></div>
+<input class="code2"type="text" placeholder="Enter your code                                              &rarr;" onchange="updateTotalPrice()" >
+<div class="line"></div>
+<div class="sp"><p class"ice">TOTALPRICE</p><p id="total1">€${totalPrice}.00</p></div>
 <button id="cout">CHECKOUT</button> </div>`;
 const cd = document.querySelector(".code2");
 
@@ -108,7 +101,8 @@ function itemQuantityP(index) {
 
   if (pq[index] > 0) {
     prev.removeAttribute("disabled");
-  }
+  }totalPrice += home.p[index].price;
+  updateTotalPrice();
 }
 
 function itemQuantityM(index) {
@@ -122,7 +116,8 @@ function itemQuantityM(index) {
 
   if (pq[index] === 0) {
     prev.setAttribute("disabled", true);
-  }
+  }totalPrice -= home.p[index].price;
+  updateTotalPrice();
 }
 
 function calcTotalPrice(q) {
@@ -135,12 +130,17 @@ function updateTotalPrice() {
   const ind = document.getElementById("inputGroupSelect04").selectedIndex;
   const totalWithDlv = calcTotalPrice(ind);
   document.getElementById("total1").textContent = `€${totalWithDlv}.00`;
+  document.getElementById("total").textContent = `€${totalPrice}.00`;
+  const discountInput = document.querySelector(".code2").value;
+
+  if (discountInput === "FULLSTACK") {
+    const totalWithDsc = Math.round((totalWithDlv / 100) * 90);
+    document.querySelector("#total1").textContent = `€${totalWithDsc}.00`;
+  }
 }
 
-function updateDiscountPrice() {
-  //document.querySelector(".code2").innerText = "";
-  //if ((document.querySelector(".code2").innerText = "FULLSTACK")) {
-    const totalWithDsc = Math.round((totalPrice / 100) * 90);
-    document.querySelector("#total1").textContent = `€${totalWithDsc}.00`;
-  //}
+updateTotalPrice();
+function zero(){pq = [0, 0, 0];
+  document.getElementById("total1").textContent = `€0.00`;
+  document.getElementById("total").textContent = `€0.00`;
 }
